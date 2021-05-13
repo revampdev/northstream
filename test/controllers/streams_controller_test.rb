4f491a55ed117919bf51abf39593f28242846d96
James Weis <jw@revamp.dev>
2021-05-13 14:14:38 -0400

    updated

--- test/controllers/streams_controller_test.rb
+++ test/controllers/streams_controller_test.rb
@@ -16,8 +16,8 @@ class StreamsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create stream" do
-    assert_difference('Stream.count') do
-      post streams_url, params: { stream: { account_id: @stream.account_id, live_stream_id: @stream.live_stream_id, name: @stream.name, playback_id: @stream.playback_id, price: @stream.price, slug: @stream.slug, status: @stream.status, stream_date: @stream.stream_date, stream_key: @stream.stream_key, stream_rtmp_link: @stream.stream_rtmp_link } }
+    assert_difference("Stream.count") do
+      post streams_url, params: {stream: {account_id: @stream.account_id, live_stream_id: @stream.live_stream_id, name: @stream.name, playback_id: @stream.playback_id, price: @stream.price, slug: @stream.slug, status: @stream.status, stream_date: @stream.stream_date, stream_key: @stream.stream_key, stream_rtmp_link: @stream.stream_rtmp_link}}
     end
 
     assert_redirected_to stream_url(Stream.last)
@@ -34,12 +34,12 @@ class StreamsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update stream" do
-    patch stream_url(@stream), params: { stream: { account_id: @stream.account_id, live_stream_id: @stream.live_stream_id, name: @stream.name, playback_id: @stream.playback_id, price: @stream.price, slug: @stream.slug, status: @stream.status, stream_date: @stream.stream_date, stream_key: @stream.stream_key, stream_rtmp_link: @stream.stream_rtmp_link } }
+    patch stream_url(@stream), params: {stream: {account_id: @stream.account_id, live_stream_id: @stream.live_stream_id, name: @stream.name, playback_id: @stream.playback_id, price: @stream.price, slug: @stream.slug, status: @stream.status, stream_date: @stream.stream_date, stream_key: @stream.stream_key, stream_rtmp_link: @stream.stream_rtmp_link}}
     assert_redirected_to stream_url(@stream)
   end
 
   test "should destroy stream" do
-    assert_difference('Stream.count', -1) do
+    assert_difference("Stream.count", -1) do
       delete stream_url(@stream)
     end
 
